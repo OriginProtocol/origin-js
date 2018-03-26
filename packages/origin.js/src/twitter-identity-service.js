a291543b4059f15d5e11109bd483ce5c3e7db7da
Tyler Yasaka <hey@tyleryasaka.me>
2018-03-26 16:02:16 -0700

    Change addProof implementation
    It no longer posts to twitter on the user's behalf. Instead it searches
    for an existing tweet with the proof.

--- packages/origin.js/src/twitter-identity-service.js
+++ packages/origin.js/src/twitter-identity-service.js
@@ -57,36 +57,45 @@ class TwitterIdentityService {
   }
 
   // TODO: set server url upon origin.js instantiation as a config option, rather than passing into this method
-  addProof(accessTokenKey, accessTokenSecret, serverUrl) {
+  addProof(username, serverUrl) {
+    let messageToTweet
     return new Promise((resolve, reject) => {
-      originService.generateSignedMessage(messageToSign)
-      .then((signedMessage) => {
-        let status = `Verifying myself on originprotocol.com: ${signedMessage}`
-        return axios.post(`${serverUrl}/api/twitter/statuses/update`, { accessTokenKey, accessTokenSecret, status })
+      this.getMessageToTweet()
+      .then((status) => {
+        messageToTweet = status
+        return axios.get(`${serverUrl}/api/twitter/statuses/user_timeline/${username}`)
       })
       .then(function (response) {
-        let tweet = response.data
-        let proofLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-        window.web3.eth.getAccounts((error, accounts) => {
-          originService.getUser(accounts[0])
-          .then((user) => {
-            let proofs = user.proofs || []
-            proofs.push({
-              service: "twitter.com",
-              username: tweet.user.screen_name,
-              proofString: status,
-              proofLink
-            })
-            user.proofs = proofs
-            return originService.setUser(user)
-          })
-          .then((response) => {
-            resolve(response)
-          })
-          .catch((error) => {
-            reject(`Error adding twitter proof: ${error}`)
-          })
+        let tweets = response.data
+        let matchingTweets = tweets.filter((t) => {
+          // the url is changed by twitter, so we can't do an exact message match
+          return t.full_text.split(': ')[1] === messageToTweet.split(': ')[1]
         })
+        if (!matchingTweets.length) {
+          reject('Tweet not found')
+        } else {
+          let tweet = matchingTweets[0]
+          let proofLink = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+          window.web3.eth.getAccounts((error, accounts) => {
+            originService.getUser(accounts[0])
+            .then((user) => {
+              user.proofs = user.proofs || []
+              user.proofs.push({
+                service: "twitter.com",
+                username: tweet.user.screen_name,
+                proofString: status,
+                proofLink
+              })
+              return originService.setUser(user)
+            })
+            .then((response) => {
+              resolve(response)
+            })
+            .catch((error) => {
+              reject(`Error adding twitter proof: ${error}`)
+            })
+          })
+        }
       })
       .catch((error) => {
         reject(`Error adding twitter proof: ${error}`)
