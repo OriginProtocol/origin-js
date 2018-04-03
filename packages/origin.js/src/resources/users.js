b2cf4afdb80610f8d7426746864d90116d8ae07d
Tyler Yasaka <hey@tyleryasaka.me>
2018-04-03 12:11:22 -0700

    Define user resource methods as standalone functions

--- packages/origin.js/src/resources/users.js
+++ packages/origin.js/src/resources/users.js
@@ -3,24 +3,27 @@ import userSchema from '../schemas/user.json'
 var Ajv = require('ajv')
 var ajv = new Ajv()
 
-module.exports = {
-  create: async function(data) {
-    let validate = ajv.compile(userSchema)
-    if (!validate(data)) {
-      throw new Error('invalid user data')
-    } else {
-      // Submit to IPFS
-      let ipfsHash = await this.origin.ipfsService.submitFile(data)
+async function create(data) {
+  let validate = ajv.compile(userSchema)
+  if (!validate(data)) {
+    throw new Error('invalid user data')
+  } else {
+    // Submit to IPFS
+    let ipfsHash = await this.origin.ipfsService.submitFile(data)
 
-      // Submit to ETH contract
-      let txReceipt = await this.origin.contractService.setUser(ipfsHash)
-      return txReceipt
-    }
-  },
-
-  get: async function(address) {
-    let userIpfsHash = await this.origin.contractService.getUser(address)
-    let userJson = await this.origin.ipfsService.getFile(userIpfsHash)
-    return userJson
+    // Submit to ETH contract
+    let txReceipt = await this.origin.contractService.setUser(ipfsHash)
+    return txReceipt
   }
+}
+
+async function get(address) {
+  let userIpfsHash = await this.origin.contractService.getUser(address)
+  let userJson = await this.origin.ipfsService.getFile(userIpfsHash)
+  return userJson
+}
+
+module.exports = {
+  create,
+  get
 }
