cd30d6092e6b5bc86a9fced08bea23f1da6e15ed
David Clements <digidigo@gmail.com>
2018-05-14 21:44:17 -0600

    Initial thoughts on deploy checklist script

---
+++ scripts/deploy_checklist.js
@@ -0,0 +1,55 @@
+var Web3 = require('web3')
+const got = require('got');
+
+let limit = 3 // need at least 3 etht to get through deploy errors
+let rinkby = 'https://rinkeby.infura.io/'
+let ropsten = 'https://ropsten.infura.io/'
+
+
+let rinkbyAccounts = process.env.RINKBY_ACCOUNTS.split(',')
+let ropstenAccounts = process.env.ROPSTEN_ACCOUNTS.split(',')
+
+let allAccounts = []
+
+rinkbyAccounts.forEach(function(address){
+  allAccounts.push({provider: rinkby, address: address})
+})
+
+ropstenAccounts.forEach(function(address){
+  allAccounts.push({provider: ropsten, address: address})
+})
+
+async function getBalances(accounts){
+  var balances = []
+
+  for (let index = 0; index < accounts.length; index++) {
+    var account = accounts[index]
+
+    var web3 = new Web3(account.provider)
+
+    var result = await web3.eth.getBalance(account.address)
+    var balance = web3.utils.fromWei(result)
+    balances.push({provider: account.provider, address: account.address, eth: balance})
+  }
+  return balances
+}
+
+getBalances(allAccounts).then(function(accounts){
+  accounts.forEach(function(account){
+    if( account.eth < limit){
+      if( account.provider == ropsten){
+        console.log(`Low Ropsten wallet loading via api: ${account.address}`)
+        got(`http://faucet.ropsten.be:3001/donate/${account.address}`,{json: true}).then(response => {
+          console.log(response.body.message || "Success");
+        }).catch(error => {
+          console.log(error)
+        })
+      } else {
+        console.log(`Low Rinkby wallet: ${account.address}`)
+        console.log(`Go to https://faucet.rinkeby.io/`)
+      }
+    } else {
+      console.log(`Good ${account.provider} wallet: ${account.address}\n`)
+    }
+  })
+})
