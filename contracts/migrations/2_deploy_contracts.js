85d55c4f94c78a29355f7af6aed74788b417d4e9
Tyler Yasaka <contact@tyleryasaka.me>
2018-05-15 20:34:35 -0700

    Use library to reduce gas costs of KeyHolder contract

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -2,6 +2,8 @@ var ClaimHolder = artifacts.require("./ClaimHolder.sol")
 var ClaimHolderLibrary = artifacts.require("./ClaimHolderLibrary.sol")
 var ClaimHolderPresigned = artifacts.require("./ClaimHolderPresigned.sol")
 var ClaimHolderRegistered = artifacts.require("./ClaimHolderRegistered.sol")
+var KeyHolder = artifacts.require("./KeyHolder.sol")
+var KeyHolderLibrary = artifacts.require("./KeyHolderLibrary.sol")
 var ListingsRegistry = artifacts.require("./ListingsRegistry.sol")
 var Listing = artifacts.require("./Listing.sol")
 var UserRegistry = artifacts.require("./UserRegistry.sol")
@@ -19,11 +21,24 @@ async function deployContracts(deployer) {
   await deployer.link(PurchaseLibrary, ListingsRegistry)
   await deployer.link(PurchaseLibrary, Listing)
   await deployer.deploy(ListingsRegistry)
+  await deployer.deploy(KeyHolderLibrary)
+
+  await deployer.link(KeyHolderLibrary, ClaimHolder)
   await deployer.deploy(ClaimHolderLibrary)
+
+  await deployer.link(KeyHolderLibrary, KeyHolder)
+
   await deployer.link(ClaimHolderLibrary, ClaimHolder)
+
   await deployer.link(ClaimHolderLibrary, ClaimHolderPresigned)
+  await deployer.link(KeyHolderLibrary, ClaimHolderPresigned)
+
   await deployer.link(ClaimHolderLibrary, ClaimHolderRegistered)
+  await deployer.link(KeyHolderLibrary, ClaimHolderRegistered)
+
   await deployer.link(ClaimHolderLibrary, OriginIdentity)
+  await deployer.link(KeyHolderLibrary, OriginIdentity)
+
   await deployer.deploy(UserRegistry)
   await deployer.deploy(OriginIdentity)
 }
