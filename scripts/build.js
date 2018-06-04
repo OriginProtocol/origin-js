2ea2c5a21973bd3c652e03ab743378946343b48a
Tyler Yasaka <contact@tyleryasaka.me>
2018-06-04 16:51:31 -0700

    Code format

--- scripts/build.js
+++ scripts/build.js
@@ -1,33 +1,33 @@
-const chalk = require('chalk');
-const startGanache = require('./helpers/start-ganache')
-const buildContracts = require('./helpers/build-contracts')
-const deployContracts = require('./helpers/deploy-contracts')
-const startIpfs = require('./helpers/start-ipfs')
-const startTestServer = require('./helpers/start-test-server')
-const watch = require('node-watch')
-const webpack = require('webpack')
-const webpackConfig = require('../webpack.config.js')
+const chalk = require("chalk")
+const startGanache = require("./helpers/start-ganache")
+const buildContracts = require("./helpers/build-contracts")
+const deployContracts = require("./helpers/deploy-contracts")
+const startIpfs = require("./helpers/start-ipfs")
+const startTestServer = require("./helpers/start-test-server")
+const watch = require("node-watch")
+const webpack = require("webpack")
+const webpackConfig = require("../webpack.config.js")
 
 const args = process.argv.slice(2)
-const shouldWatch = (args.length && args[0] === 'serve')
-const noGanache = (args.length && args[1] === 'no-ganache')
+const shouldWatch = (args.length && args[0] === "serve")
+const noGanache = (args.length && args[1] === "no-ganache")
 
 const start = async () => {
-  let compiler = webpack(webpackConfig)
+  const compiler = webpack(webpackConfig)
 
   if (shouldWatch) {
     if (!noGanache) {
-      console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Starting Local Blockchain }\n`);
+      console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Starting Local Blockchain }\n`)
       await startGanache()
     }
-    console.log(chalk`\n{bold.hex('#26d198') ⬢  Deploying Smart Contracts }\n`);
+    console.log(chalk`\n{bold.hex('#26d198') ⬢  Deploying Smart Contracts }\n`)
     await deployContracts()
-    console.log(chalk`\n{bold.hex('#6e3bea') ⬢  Starting Local IPFS }\n`);
+    console.log(chalk`\n{bold.hex('#6e3bea') ⬢  Starting Local IPFS }\n`)
     await startIpfs()
 
     // watch contracts
-    watch('./contracts/contracts', { recursive: true }, (evt, name) => {
-      console.log('%s changed.', name)
+    watch("./contracts/contracts", { recursive: true }, (evt, name) => {
+      console.log("%s changed.", name)
       deployContracts()
     })
 
@@ -44,17 +44,17 @@ const start = async () => {
       }
     })
 
-    console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Starting Test Server }\n`);
+    console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Starting Test Server }\n`)
     startTestServer()
   } else {
-    console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Compiling Smart Contracts }\n`);
+    console.log(chalk`\n{bold.hex('#1a82ff') ⬢  Compiling Smart Contracts }\n`)
     await buildContracts()
-    console.log(chalk`\n{bold.hex('#26d198') ⬢  Compiling Webpack }\n`);
-    compiler.run((err, stats) => {
+    console.log(chalk`\n{bold.hex('#26d198') ⬢  Compiling Webpack }\n`)
+    compiler.run((err) => {
       if(err) {
         console.log(err)
       } else {
-        console.log('webpack compiled successfully')
+        console.log("webpack compiled successfully")
       }
     })
   }
