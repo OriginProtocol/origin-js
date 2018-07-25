1f779fba62f2e88c4b6a8f23c1f287f4727c566f
Daniel Von Fange <daniel@leancoder.com>
2018-07-25 10:26:34 -0400

    EvolvingRegistry - test access control

--- contracts/test/TestEvolvingRegistry.js
+++ contracts/test/TestEvolvingRegistry.js
@@ -26,11 +26,9 @@ contract('EvolvingRegistry', accounts => {
     it('should not allow a stranger to transfer ownership', async function() {
       try {
         await registry.transferOwnership(secondOwner, { from: stranger })
-      } catch (e) {
-        assert.equal(await registry.owner(), owner)
-        return
-      }
-      assert(false, 'should not allow ownership transfer')
+        assert(false)
+      } catch (e) {}
+      assert.equal(await registry.owner(), owner)
     })
   })
 
@@ -52,6 +50,15 @@ contract('EvolvingRegistry', accounts => {
       assert.equal(listingType[1], 'Listings_V0')
       assert.equal(listingType[2], true)
     })
+    it('should not allow a stranger to add an entry type', async () => {
+      const listingType = await registry.getEntryType(0)
+      try {
+        await registry.addEntryType(listingsA.address, 'Listings_VEVIL', {
+          from: stranger
+        })
+        assert(false)
+      } catch (e) {}
+    })
     it('should enable an entry type', async () => {
       let listingType
       // Disable the type
@@ -63,19 +70,52 @@ contract('EvolvingRegistry', accounts => {
       listingType = await registry.getEntryType(0)
       assert.equal(listingType[2], true)
     })
+    it('should not allow a stranger to enable an entry type', async () => {
+      let listingType
+      // Disable the type
+      await registry.disableEntryType(0, { from: owner })
+      listingType = await registry.getEntryType(0)
+      assert.equal(listingType[2], false)
+      // Renable it
+      try {
+        await registry.enableEntryType(0, { from: stranger })
+        assert(false)
+      } catch (e) {}
+      listingType = await registry.getEntryType(0)
+      assert.equal(listingType[2], false)
+    })
     it('should disable an entry type', async () => {
       await registry.disableEntryType(0, { from: owner })
       const listingType = await registry.getEntryType(0)
       assert.equal(listingType[2], false)
+    })
+    it('should not allow a stranger to disable an entry type', async () => {
+      try {
+        await registry.disableEntryType(0, { from: stranger })
+        assert(false)
+      } catch (e) {}
+      const listingType = await registry.getEntryType(0)
+      assert.equal(listingType[2], true)
     })
     it('should rename an entry type', async () => {
       await registry.renameEntryType(0, 'FooBar', { from: owner })
       const listingType = await registry.getEntryType(0)
       assert.equal(listingType[1], 'FooBar')
     })
+    it('should not allow a stranger to rename an entry type', async () => {
+      try {
+        await registry.renameEntryType(0, 'FooBar', { from: stranger })
+        assert(false)
+      } catch (e) {}
+      const listingType = await registry.getEntryType(0)
+      assert.equal(listingType[1], 'Listings_V0')
+    })
   })
 
   describe('Adding Entries', async () => {
+    let registry
+    let listingsA
+
     beforeEach(async () => {
       registry = await EvolvingRegistry.new({ from: owner })
       listingsA = await Listings.new(registry.address, { from: owner })
@@ -88,6 +128,25 @@ contract('EvolvingRegistry', accounts => {
       const listingType = await registry.getEntryTypeOfEntry(0)
       assert(listingType[0], listingsA.address)
       assert(await registry.size(), 1)
+    })
+    it('should not allow an entry from a disabled contract', async () => {
+      await registry.disableEntryType(0, { from: owner })
+      try {
+        await listingsA.createListing(IPFS_HASH, { from: seller })
+        console.log(await registry.size())
+        assert(false)
+      } catch (e) {}
+      assert(await registry.size(), 0)
+    })
+    it("should not allow a stranger's listing contract to add an entry", async () => {
+      const evilListings = await Listings.new(registry.address, {
+        from: stranger
+      })
+      try {
+        await evilListings.createListing(IPFS_HASH, { from: seller })
+        assert(false)
+      } catch (e) {}
+      assert(await registry.size(), 0)
     })
   })
 })
