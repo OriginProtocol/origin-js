309102c36bad0a9744cc3206cd3b3023abd62976
Tyler Yasaka <contact@tyleryasaka.me>
2018-07-27 23:20:46 -0700

    Update reviews resource to work with new contracts

--- src/resources/reviews.js
+++ src/resources/reviews.js
@@ -1,83 +1,72 @@
-import ResourceBase from './_resource-base'
+const buyerReviewStage = 4
+const sellerReviewStage = 5
+const reviewStages = [buyerReviewStage, sellerReviewStage]
+const emptyIpfsHash = '0x0000000000000000000000000000000000000000000000000000000000000000'
 
-class Reviews extends ResourceBase {
+class Reviews {
   constructor({ contractService, ipfsService }) {
-    super({ contractService, ipfsService })
+    this.contractService = contractService
+    this.ipfsService = ipfsService
     this.contractDefinition = this.contractService.purchaseContract
   }
 
   async find(where = {}) {
-    if (where.purchaseAddress) {
-      return await this._find_by_purchase_id(where.purchaseAddress)
+    const hasListingId = where.listingId !== undefined
+    const hasPurchaseId = where.purchaseId !== undefined
+    if (hasListingId && hasPurchaseId) {
+      return await this._find_by_purchase_id(where.listingId, where.purchaseId)
     }
   }
 
   // Temporary, until bridge server intergration
-  async _find_by_purchase_id(address) {
-    const self = this
-    const web3 = this.contractService.web3
-    const purchaseDefinition = this.contractService.purchaseContract
-    const purchaseContract = new web3.eth.Contract(
-      purchaseDefinition.abi,
-      address
+  async _find_by_purchase_id(listingId, purchaseId) {
+    const v01_ListingsContract = await this.contractService.deployed(
+      this.contractService.v01_ListingsContract
     )
-    return new Promise((resolve, reject) => {
-      // Get all logs on this contract
-      purchaseContract.getPastEvents('allEvents', { fromBlock: 0 }, function(
-        error,
-        rawLogs
-      ) {
-        if (error) {
-          return reject(error)
-        }
-        // Format logs we receive
-        const logs = rawLogs
-          .filter(x => x.event == 'PurchaseReview')
-          .map(log => {
-            const values = log.returnValues
-            const hasHash =
-              values.ipfsHash !=
-              '0x0000000000000000000000000000000000000000000000000000000000000000'
-            const ipfsHash = hasHash
-              ? self.contractService.getIpfsHashFromBytes32(values.ipfsHash)
-              : undefined
-            return {
-              purchaseAddress: address,
-              reviewerAddress: values.reviewer,
-              revieweeAddress: values.reviewee,
-              revieweeRole: ['BUYER', 'SELLER'][values.revieweeRole],
-              rating: parseInt(values.rating),
-              ipfsHash: ipfsHash,
-              reviewText: '',
-              transactionHash: log.transactionHash,
-              blockNumber: log.blockNumber,
-              blockHash: log.blockHash
-            }
-          })
-        // Fetch user and timestamp information for all logs, in parallel
-        const addReviewDetails = async event => {
-          if (event.ipfsHash == undefined) {
-            return
+    const logs = await new Promise((resolve, reject) => {
+      v01_ListingsContract.getPastEvents(
+        'PurchaseChange',
+        {
+          fromBlock: 0,
+          toBlock: 'latest',
+          filter: { _listingIndex: listingId, _purchaseIndex: purchaseId }
+        },
+        function(error, logs) {
+          if (error) {
+            reject(error)
           }
-          const data = await self.ipfsService.getFile(event.ipfsHash)
-          event.reviewText = data.reviewText
-        }
-        const addTimestampFn = async event => {
-          event.timestamp = (await self.contractService.getBlock(
-            event.blockHash
-          )).timestamp
-        }
-        const fetchPromises = [].concat(
-          logs.map(addReviewDetails),
-          logs.map(addTimestampFn)
-        )
-        Promise.all(fetchPromises)
-          .then(() => {
-            resolve(logs)
-          })
-          .catch(error => reject(error))
+        resolve(logs)
       })
     })
+    const itemsPromise = logs
+      .filter(log => {
+        return reviewStages.includes(Number(log.returnValues._stage))
+      })
+      .map((log) => this.createItem(log))
+    const items = await Promise.all(itemsPromise)
+    const buyerReviewItems = items.filter(({ stage }) => {
+      return Number(stage) === buyerReviewStage
+    })
+    const sellerReviewItems = items.filter(({ stage }) => {
+      return Number(stage) === sellerReviewStage
+    })
+    const format = (item) => {
+      return { ipfsData: item.ipfsData }
+    }
+    const fromBuyer = buyerReviewItems.length ? format(buyerReviewItems[0]) : null
+    const fromSeller = sellerReviewItems.length ? format(sellerReviewItems[0]) : null
+    return { fromBuyer, fromSeller }
+  }
+
+  async createItem(log) {
+    const asBytes32 = log.returnValues._ipfsHash
+    const stage = log.returnValues._stage
+    const ipfsHash = this.contractService.getIpfsHashFromBytes32(asBytes32)
+    let ipfsData
+    if (ipfsHash !== emptyIpfsHash) {
+      ipfsData = await this.ipfsService.getFile(ipfsHash)
+    }
+    return { ipfsData, stage }
   }
 }
 
---
+++ test/resource_reviews.test.js
@@ -0,0 +1,101 @@
+import Listings from '../src/resources/listings.js'
+import Purchase from '../src/resources/purchases.js'
+import Reviews from '../src/resources/reviews'
+import IpfsService from '../src/services/ipfs-service'
+import { expect } from 'chai'
+import Web3 from 'web3'
+import asAccount from './helpers/as-account'
+import contractServiceHelper from './helpers/contract-service-helper'
+
+const samplePrice = 1000000000000000
+const listingData = {
+  priceWei: samplePrice,
+  unitsAvailable: 1,
+  listingType: 'unit',
+  name: 'Foo Bar'
+}
+const purchaseData = {
+  priceWei: samplePrice,
+  units: 1,
+  purchaseType: 'unit'
+}
+
+describe('Review Resource', function() {
+  this.timeout(5000) // default is 2000
+
+  let reviews,
+    accounts,
+    seller,
+    buyer,
+    contractService,
+    listings,
+    listingIndex,
+    purchases,
+    purchaseIndex
+
+  beforeEach(async () => {
+    const ipfsService = new IpfsService({
+      ipfsDomain: '127.0.0.1',
+      ipfsApiPort: '5002',
+      ipfsGatewayPort: '8080',
+      ipfsGatewayProtocol: 'http'
+    })
+    const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+    const web3 = new Web3(provider)
+    contractService = await contractServiceHelper(web3)
+    accounts = await web3.eth.getAccounts()
+    seller = accounts[0]
+    buyer = accounts[2]
+    listings = new Listings({ contractService, ipfsService })
+    purchases = new Purchase({ contractService, ipfsService })
+    reviews = new Reviews({ contractService, ipfsService })
+
+    await listings.create(listingData)
+    const listingIds = await listings.allIds()
+    listingIndex = listingIds[listingIds.length - 1]
+
+    await asAccount(contractService.web3, buyer, async () => {
+      return await listings.requestPurchase(
+        listingIndex,
+        purchaseData,
+        samplePrice
+      )
+    })
+
+    const listingPurchases = await listings.getPurchases(listingIndex)
+    purchaseIndex = listingPurchases.length - 1
+    await purchases.acceptRequest(
+      listingIndex,
+      purchaseIndex,
+      {}
+    )
+  })
+
+  describe('find', () => {
+    it('should return review data', async () => {
+      await asAccount(contractService.web3, buyer, async () => {
+        return await purchases.buyerFinalize(
+          listingIndex,
+          purchaseIndex,
+          { foo: 'bar' }
+        )
+      })
+      let results = await reviews.find({
+        listingId: listingIndex,
+        purchaseId: purchaseIndex
+      })
+      expect(results.fromBuyer.ipfsData.foo).to.equal('bar')
+
+      await purchases.sellerFinalize(
+        listingIndex,
+        purchaseIndex,
+        { crypto: 'hodl' }
+      )
+      results = await reviews.find({
+        listingId: listingIndex,
+        purchaseId: purchaseIndex
+      })
+      expect(results.fromSeller.ipfsData.crypto).to.equal('hodl')
+    })
+  })
+})
