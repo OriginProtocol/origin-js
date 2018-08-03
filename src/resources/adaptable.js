b25edb5165bc517a5adfdac3f605ad75d7409cd9
Tyler Yasaka <contact@tyleryasaka.me>
2018-08-02 19:38:41 -0700

    Keep track of contract versions in origin-js

--- src/resources/adaptable.js
+++ src/resources/adaptable.js
@@ -1,26 +1,24 @@
 import v01ListingsAdapter from '../adapters/v01/listings_adapter'
+import { parseListingId } from '../utils/id'
 
 class Adaptable {
   constructor() {
     this.adapters = {
-      V01_Listings: new v01ListingsAdapter(...arguments)
+      '001': new v01ListingsAdapter(...arguments)
     }
-    this.currentAdapter = this.adapters.V01_Listings
+    this.versions = [
+      '001'
+    ]
+    this.currentVersion = this.versions[this.versions.length - 1]
+    this.currentAdapter = this.adapters[this.currentVersion]
   }
 
-  async getAdapter(listingIndex) {
-    // get entry type of listing from evolving registry
-    const entryType = await this.contractService.call(
-      'evolvingRegistryContract',
-      'getEntryTypeOfEntry',
-      [listingIndex]
-    )
-    const entryTypeName = entryType['_name']
-
-    // use appropriate adapter for entry type
-    const adapter = this.adapters[entryTypeName]
+  async getAdapter(listingId) {
+    const { version } = parseListingId(listingId)
+    // use appropriate adapter for version
+    const adapter = this.adapters[version]
     if (!adapter) {
-      throw new Error(`Adapter not found: ${entryTypeName}`)
+      throw new Error(`Adapter not found for version ${version}`)
     }
     return adapter
   }
