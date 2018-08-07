81b7bb0ba029859b301643d910a3b9ed5fd72ebb
Tyler Yasaka <contact@tyleryasaka.me>
2018-08-06 20:38:05 -0700

    Add offer id parser/generator

--- src/utils/id.js
+++ src/utils/id.js
@@ -16,4 +16,25 @@ function generateListingId({ version, network, listingIndex }) {
   return [ network, version, listingIndex ].join(separator)
 }
 
-module.exports = { parseListingId, generateListingId }
+function parseOfferId(offerId) {
+  if (typeof offerId !== 'string') {
+    throw new Error(`Listing id ${listingId} must be a string`)
+  }
+  const exploded = offerId.split(separator)
+  if (exploded.length !== 4) {
+    throw new Error(`Invalid offer id: ${offerId}`)
+  }
+  const [ network, version, listingIndex, offerIndex ] = exploded
+  return { network, version, listingIndex, offerIndex }
+}
+
+function generateOfferId({ version, network, listingIndex, offerIndex }) {
+  return [ network, version, listingIndex, offerIndex ].join(separator)
+}
+
+module.exports = {
+  parseListingId,
+  generateListingId,
+  parseOfferId,
+  generateOfferId
+}
