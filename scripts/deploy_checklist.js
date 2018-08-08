fa7d64af53cb3c3bf627d5c33766ee050359e1f0
Stan James <stan@wanderingstan.com>
2018-08-08 09:07:10 -0600

    Trimmed to min functionality

--- scripts/deploy_checklist.js
+++ scripts/deploy_checklist.js
@@ -1,68 +1,29 @@
-var Web3 = require('web3')
-var HDWalletProvider = require("truffle-hdwallet-provider")
-
+let Web3 = require('web3')
+let HDWalletProvider = require("truffle-hdwallet-provider")
 const got = require('got')
 
-let mnemonic = process.env.MNEMONIC
-
-let minimumEthForDeploy = 3 // need at least 3 eth to get through deploy errors
-let addressCount = 4
-let rinkby = 'https://rinkeby.infura.io/'
-let ropsten = 'https://ropsten.infura.io/'
-
-let allAccounts = []
-
-var provider = new HDWalletProvider(mnemonic, rinkby, 0, addressCount)
-var web3 = new Web3()
-web3.setProvider(provider)
-allAccounts.push({ provider: rinkby, web3: web3 })
-
-var provider = new HDWalletProvider(mnemonic, ropsten, 0, addressCount)
-var web3 = new Web3()
-web3.setProvider(provider)
-allAccounts.push({ provider: ropsten, web3: web3 })
-
-async function getBalances(providers) {
-  var balances = []
-
-  for (var index = 0; index < providers.length; index++) {
-    var provider = providers[index]
-
-    var web3 = provider.web3
-    var accounts = await web3.eth.getAccounts()
-
-    for (var i = 0; i < accounts.length; i++) {
-      var account = accounts[i]
-      var result = await web3.eth.getBalance(account)
-      var balance = web3.utils.fromWei(result)
-      balances.push({ provider: provider.provider, address: account, eth: balance })
-    }
-  }
-  return balances
+if (!process.env.RINKEBY_MNEMONIC) {
+  console.error("RINKEBY_MNEMONIC is not set.")
+  process.exit()
 }
+let mnemonic = process.env.RINKEBY_MNEMONIC
+let ethNodeUrl = 'https://rinkeby.infura.io/'
+let addressCount = 2 // How many addresses to handle in walet
+
+let provider = new HDWalletProvider(mnemonic, ethNodeUrl, 0, addressCount)
+let web3 = new Web3()
+web3.setProvider(provider)
 
 const run = async () => {
-  var accounts = await getBalances(allAccounts)
+  var networkType = await web3.eth.net.getNetworkType()
+  console.log(`Network: ${networkType}`)
+  var accounts = await web3.eth.getAccounts()
   for (var i = 0; i < accounts.length; i++) {
     var account = accounts[i]
-    if ( account.eth < minimumEthForDeploy) {
-      if ( account.provider == ropsten) {
-        console.log(`\nLow Ropsten wallet loading via api: ${account.address}`)
-        try {
-          var response = await got(`http://faucet.ropsten.be:3001/donate/${account.address}`, {json: true})
-          console.log(response.body.message || "Success")
-        } catch(error) {
-          console.log(error)
-        }
-      } else {
-          console.log(`\nLow Rinkby wallet: ${account.address}`)
-          console.log(`Go to https://faucet.rinkeby.io/ for ${account.address}`)
-      }
-    } else {
-      console.log(`\nGood ${account.provider} wallet: ${account.address}`)
-    }
+    var result = await web3.eth.getBalance(account)
+    var balance = web3.utils.fromWei(result)
+    console.log(`${i}\t${account}\t${balance}`)
   }
-  return
 }
 
 run().then(function(){ process.exit() })
