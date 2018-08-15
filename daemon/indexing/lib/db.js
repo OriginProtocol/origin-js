b378c9adbc999224330485ecd47e5d2f50d81c2a
Daniel Von Fange <daniel@leancoder.com>
2018-08-15 09:49:57 -0400

    DB inserting should be idempotent.
    It should be fine to rerun the listener and reindex all listings.

--- daemon/indexing/lib/db.js
+++ daemon/indexing/lib/db.js
@@ -55,8 +55,10 @@ class Listing {
    * @returns The listingId indexed.
    */
   static async insert(listingId, listing) {
+    // TODO: Check that we are not replacing new data with old
     const res = await pool.query(
-      `INSERT INTO ${Listing.table}(id, data) VALUES($1, $2)`, [listingId, listing])
+      `INSERT INTO ${Listing.table}(id, data) VALUES($1, $2)
+      ON CONFLICT (id) DO UPDATE SET data = excluded.data`, [listingId, listing])
     console.log(`Added row ${listingId} to listing table.`)
     return listingId
   }
