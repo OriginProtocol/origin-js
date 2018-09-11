9c412a5f1a71d542b9d811c9aad056876b3da7c3
Domen Grabec <grabec@gmail.com>
2018-09-11 10:27:57 +0200

    minor formatting fix

--- scripts/helpers/pre-build.js
+++ scripts/helpers/pre-build.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const fsExtra = require('fs-extra')
 
 module.exports = function() {
-  if (!fs.existsSync(".env")) {
+  if (!fs.existsSync('.env')) {
     console.log(`\nFile .env does not exist. Copying contents from dev.env to .env\n`)
     fsExtra.copySync('dev.env', '.env')
   }
