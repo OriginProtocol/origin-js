dd19f17373fb77c30ad6074684bfd26f120727eb
Yu Pan <yupan@originprotocol.com>
2018-09-14 14:38:20 -0700

    Added approveAndCallWithSender to contract and migration

--- contracts/contracts/token/OriginToken.sol
+++ contracts/contracts/token/OriginToken.sol
@@ -14,6 +14,11 @@ contract OriginToken is BurnableToken, MintableToken, WhitelistedPausableToken {
   string public constant symbol = "OGN"; // solium-disable-line uppercase
   uint8 public constant decimals = 18; // solium-disable-line uppercase
 
+  event AddCallSpenderWhitelist(address enabler, address spender);
+  event RemoveCallSpenderWhitelist(address disabler, address spender);
+
+  mapping (address => bool) public callSpenderWhitelist;
+
   // @dev Constructor that gives msg.sender all initial tokens.
   constructor(uint256 initialSupply) public {
     owner = msg.sender;
@@ -38,4 +43,35 @@ contract OriginToken is BurnableToken, MintableToken, WhitelistedPausableToken {
   function burn(address _who, uint256 _value) public onlyOwner {
     _burn(_who, _value);
   }
+
+  // whitelist would contain addresses for all active marketplace contracts
+  function addCallSpenderWhitelist(address _spender) public onlyOwner {
+      callSpenderWhitelist[_spender] = true;
+      emit AddCallSpenderWhitelist(msg.sender, _spender);
+  }
+
+  function removeCallSpenderWhitelist(address _spender) public onlyOwner {
+      delete callSpenderWhitelist[_spender];
+      emit RemoveCallSpenderWhitelist(msg.sender, _spender);
+  }
+
+  function approveAndCallWithSender(
+        address _spender,
+        uint256 _value,
+        bytes4 selector,
+        bytes call_params
+    )
+        public
+        payable
+        returns (bool)
+    {
+      require(_spender != address(this), "token contract can't be approved");
+      require(callSpenderWhitelist[_spender], "sender not in whitelist");
+
+      super.approve(_spender, _value);
+
+      bytes memory call_data = abi.encodePacked(selector, uint256(msg.sender), call_params);
+      require(_spender.call.value(msg.value)(call_data), "proxied call failed");
+      return true;
+    }
 }
