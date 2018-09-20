1c552ef36b3cf2a774bbb1997ae4ff31e141d98d
John <jordajm@gmail.com>
2018-09-20 15:53:53 -0600

    Hopefully fixing an error we see on deployment

--- src/contractInterface/marketplace/v00_adapter.js
+++ src/contractInterface/marketplace/v00_adapter.js
@@ -204,7 +204,7 @@ class V00_MarkeplaceAdapter {
     // Find all events related to this listing
     const listingTopic = this.padTopic(listingId)
     const events = await this.contract.getPastEvents('allEvents', {
-      topics: [null, null, listingTopic, null],
+      topics: [null, null, listingTopic],
       fromBlock: 0
     })
 
