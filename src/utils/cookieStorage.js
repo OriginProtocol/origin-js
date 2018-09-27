2f8eec9d7ee56df0c5c7c672f1444e96b35b50d3
Micah Alcorn <mail@micahalcorn.com>
2018-09-27 12:16:05 -0700

    Satisfy test

--- src/utils/cookieStorage.js
+++ src/utils/cookieStorage.js
@@ -3,7 +3,7 @@
 const DEFAULT_SECONDS_TIMEOUT = 3600 * 24 * 7
 
 export default class cookieStorage {
-  constructor({path, expireSeconds = DEFAULT_SECONDS_TIMEOUT}) {
+  constructor({ path, expireSeconds = DEFAULT_SECONDS_TIMEOUT }) {
     this.path = path
     this.expireSeconds = expireSeconds
     const cookies = document && document.cookie.match(/\=/g)
@@ -11,29 +11,58 @@ export default class cookieStorage {
   }
 
   getItem(sKey) {
-    if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
-    return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+    if (!sKey || !this.hasOwnProperty(sKey)) {
+      return null
+    }
+    return unescape(
+      document.cookie.replace(
+        new RegExp(
+          '(?:^|.*;\\s*)' +
+            escape(sKey).replace(/[\-\.\+\*]/g, '\\$&') +
+            '\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*'
+        ),
+        '$1'
+      )
+    )
   }
 
   key(nKeyId) {
-    return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+    return unescape(
+      document.cookie
+        .replace(/\s*\=(?:.(?!;))*$/, '')
+        .split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]
+    )
   }
 
   setItem(sKey, sValue) {
-    if(!sKey) { return; }
-    const expires = new Date();
-    expires.setTime(expires.getTime() + 1000 * this.expireSeconds);
-    document.cookie = escape(sKey) + "=" + escape(sValue) + '; expires=' + expires.toGMTString() + '; path=' + this.path
-    this.length = document.cookie.match(/\=/g).length;
+    if (!sKey) {
+      return
+    }
+    const expires = new Date()
+    expires.setTime(expires.getTime() + 1000 * this.expireSeconds)
+    document.cookie =
+      escape(sKey) +
+      '=' +
+      escape(sValue) +
+      '; expires=' +
+      expires.toGMTString() +
+      '; path=' +
+      this.path
+    this.length = document.cookie.match(/\=/g).length
   }
 
-  removeItem (sKey) {
-    if (!sKey || !this.hasOwnProperty(sKey)) { return; }
-    document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-    this.length--;
+  removeItem(sKey) {
+    if (!sKey || !this.hasOwnProperty(sKey)) {
+      return
+    }
+    document.cookie =
+      escape(sKey) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/'
+    this.length--
   }
 
-  hasOwnProperty (sKey) {
-    return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  hasOwnProperty(sKey) {
+    return new RegExp(
+      '(?:^|;\\s*)' + escape(sKey).replace(/[\-\.\+\*]/g, '\\$&') + '\\s*\\='
+    ).test(document.cookie)
   }
 }
